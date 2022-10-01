a89bd38ba6e54a07d8e0d9d7c76f186b5ba1a424
eddyFlores <114193534+FDEYD@users.noreply.github.com>
2022-10-01 17:37:41 -0400

    initial commit

---
+++ js/compra.js
@@ -0,0 +1,54 @@
+const compra = new Carrito();
+const listaCompra = document.querySelector("#lista-compra tbody");
+const carrito = document.getElementById('carrito');
+const procesarCompraBtn = document.getElementById('procesar-compra');
+const cliente = document.getElementById('cliente');
+const correo = document.getElementById('correo');
+
+
+cargarEventos();
+
+function cargarEventos() {
+    document.addEventListener('DOMContentLoaded', compra.leerLocalStorageCompra());
+
+    //Eliminar productos del carrito
+    carrito.addEventListener('click', (e) => { compra.eliminarProducto(e) });
+
+    compra.calcularTotal();
+
+    //cuando se selecciona procesar Compra
+    procesarCompraBtn.addEventListener('click', procesarCompra);
+
+    carrito.addEventListener('change', (e) => { compra.obtenerEvento(e) });
+    carrito.addEventListener('keyup', (e) => { compra.obtenerEvento(e) });
+
+
+}
+
+function procesarCompra() {
+    // e.preventDefault();
+    if (compra.obtenerProductosLocalStorage().length === 0) {
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'No hay productos, selecciona alguno',
+            showConfirmButton: true,
+            timer: 5000
+        }).then(function () {
+            window.location = "index.html";
+        })
+    }
+    else if (cliente.value === 'eddy flores' || correo.value === 'pepito@gmail.com') {
+        Swal.fire({
+            type: 'info',
+            title: 'Genial...',
+            text: 'Realizaste,una compra',
+            showConfirmButton: true,
+            time: 9000
+            
+            
+        })
+    }
+
+}
+
